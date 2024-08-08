7968303d760665a5079c253dc079ce38e945458e
Nick Lackman <nick.lackman@nicklackmanW93KGJYW5Y.local>
2024-08-08 15:57:59 -0700

    user input

--- python-refresh/user-input/code.py
+++ python-refresh/user-input/code.py
@@ -1,1 +1,8 @@
 name = input('Enter your name: ')
+print(name)
+
+
+size_input = input("How big is your house (SQFT): ")
+square_feet = int(size_input)
+square_metters = square_feet / 10.8
+print(f"{square_feet} square feet is equals to {square_metters:.2f} square meters")
