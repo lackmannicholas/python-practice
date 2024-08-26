70d1db7c72d3e2d6d7624e6914784d1a3fa96b27
Nick Lackman <lackmannicholas@gmail.com>
2024-08-26 16:01:59 -0700

    removing unused type

--- APIs/NewsApiRequests.py
+++ APIs/NewsApiRequests.py
@@ -1,12 +1,11 @@
 import requests
-from typing import LiteralString
 
 class NewsApiRequests:
     def __init__(self, api_key: str) -> None:
         self.__api_host__ =  f"https://newsapi.org/v2/everything?apiKey={api_key}"
         pass
 
-    def get(self, query_string: str, from_date: str = "2024-08-01", to_date: str = "2024-08-24") -> dict[str, object]:
+    def get(self, query_string: str, from_date: str = "2024-08-01", to_date: str = "2024-08-24") -> dict:
         url = self.__api_host__ + f"&qInTitle={query_string}&from={from_date}&to={to_date}"
         response = requests.get(url)
 
